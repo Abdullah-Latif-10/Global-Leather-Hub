87f92e9200e6545f4a573c57a79826a300dde15c
Ahmad <crisitiano678@gmail.com>
2026-05-01 15:26:25 +0500

    auth Middleware bugs fixed

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -57,4 +57,23 @@ const authorize = (...roles) => {
   };
 };
 
-module.exports = { protect, authorize };
+const optionalAuth = async (req, res, next) => {
+  try {
+    let token;
+    if (req.headers.authorization?.startsWith('Bearer ')) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    if (token) {
+      const decoded = verifyAccessToken(token);
+      const user = await User.findById(decoded.id);
+      if (user) {
+        req.user = user;
+      }
+    }
+  } catch (error) {
+    // ignore
+  }
+  next();
+};
+
+module.exports = { protect, authorize, optionalAuth };
