6b2495ad80a466cd77eaeec8a6b1a34c2912ef35
Ahmad <crisitiano678@gmail.com>
2026-05-02 17:39:20 +0500

    USer routes updated

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -10,9 +10,6 @@ const {
   updateProfile,
   uploadProfileAvatar,
   updatePreferredCurrency,
-  addShippingProfile,
-  updateShippingProfile,
-  deleteShippingProfile,
   getUserOrders,
   getUserOrderById,
 } = require('../controllers/userController');
@@ -39,9 +36,6 @@ router.patch('/me/username', updateUsernameValidator, validate, updateUsername);
 router.patch('/me/profile', updateProfile);
 router.patch('/me/avatar', uploadImage.single('avatar'), uploadProfileAvatar);
 router.patch('/me/preferred-currency', updatePreferredCurrency);
-router.post('/me/shipping-profiles', addShippingProfile);
-router.patch('/me/shipping-profiles/:profileId', updateShippingProfile);
-router.delete('/me/shipping-profiles/:profileId', deleteShippingProfile);
 router.post('/me/request-password-change', passwordChangeLimiter, requestPasswordChange);
 router.patch('/me/change-password', passwordChangeLimiter, changePasswordValidator, validate, changePassword);
 
